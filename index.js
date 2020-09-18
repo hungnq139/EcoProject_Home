92bc5d53168e9d9c770b205813d32aa0c1cb82cc
Hung Nguyen <quang-hung.nguyen@QE366-EA.local>
2020-09-18 16:32:48 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// https://raw.githubusercontent.com/benevbright/react-navigation-sample/master/src/LoginScreen.js
+
+import React, {useEffect} from 'react';
+import {FlatList, View, TouchableOpacity, Text, Image} from 'react-native';
+
+const Home = () => {
+  useEffect(() => {
+    console.log('moe');
+  }, []);
+  return (
+    <View style={{flex: 1, justifyContent: 'center'}}>
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate('login')}
+        style={{alignSelf: 'center'}}>
+        <Text style={{fontSize: 36}}>Moas</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+Home = Home;
+
+export default Home;
